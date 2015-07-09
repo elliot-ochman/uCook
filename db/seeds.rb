7f192123160eae56a863a65cc0e853229fe50eb7
tochman <thomas@nocebo.se>
2015-07-09 23:02:29 +0200

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Recept.create(title: 'Pannkakor', description: 'Jätte goda pannkakor')
+Recept.create(title: 'Plättar', description: 'Plättar som mättar')
+
+Ingredient.create(name: 'Mjöl')
+Ingredient.create(name: 'Ägg')
+
+
+
